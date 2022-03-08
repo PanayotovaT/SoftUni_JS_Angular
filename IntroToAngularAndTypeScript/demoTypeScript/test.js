62aa32b174cfe865a0d191d70ad0a900b2cdb58f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-09 00:36:56 +0200

    interface added

--- IntroToAngularAndTypeScript/demoTypeScript/test.js
+++ IntroToAngularAndTypeScript/demoTypeScript/test.js
@@ -1,3 +1,30 @@
-var isOpen1;
-isOpen1 = true;
-console.log(isOpen1);
+// let isOpen: boolean;
+// let list: number[] = [1, 2, 3];
+// isOpen = true;
+// console.log(isOpen);
+var Person1 = /** @class */ (function () {
+    function Person1(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    // getAge(): void {
+    //     return void this.age;
+    //     //void returns undefined. We put it when we don't want a funtion to return a result
+    // }
+    Person1.prototype.getAge = function () {
+        return this.age;
+    };
+    Person1.prototype.setData = function (newData) {
+        this.age = newData.newAge;
+        // this.name =newData.newName;
+    };
+    return Person1;
+}());
+var alex1 = new Person1('Alex', 3);
+alex1.setData({ newAge: 5, newName: 'Alexander' });
+console.log(alex1);
+function identify1(arg) {
+    return arg;
+}
+var value1 = 1;
+var test1 = identify1(value1);
--- IntroToAngularAndTypeScript/demoTypeScript/test.ts
+++ IntroToAngularAndTypeScript/demoTypeScript/test.ts
@@ -1,17 +1,23 @@
 // let isOpen: boolean;
 
+
 // let list: number[] = [1, 2, 3];
 
 // isOpen = true;
 // console.log(isOpen);
 
+interface IUserNewData {
+    newName: string; 
+    newAge: number;
+}
+
 class Person {
     constructor(
         public name: string, 
-        protected age: number,
+        protected age: number
         // public age: number,
         // protected gender: string,
-        ) {}
+        ) { }
     // getAge(): void {
     //     return void this.age;
     //     //void returns undefined. We put it when we don't want a funtion to return a result
@@ -20,17 +26,21 @@ class Person {
         return this.age;
     }
 
-    setData(newData: { newName: string, newAge: number}) {
+    setData(newData: IUserNewData) {
         this.age = newData.newAge;
-        // this.name =newData.newName;
+        this.name =newData.newName;
+    }
+    info() {
+        return `${this.name} ${this.age}`
     }
 }
 
 let alex = new Person('Alex', 3);
-alex.setData({newAge:5, newName: 'Alexander'});
+let sofi = new Person('Sofi', 2);
+const newData: IUserNewData = {newAge:5, newName: 'Alexander'}
+alex.setData(newData);
 
-// console.log(alex);
-
+// console.log(alex)
 function identify<T> (arg: T): T {
     return arg;
 }
