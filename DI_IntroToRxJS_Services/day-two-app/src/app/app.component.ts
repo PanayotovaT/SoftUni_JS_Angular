d3cf8c3ffa492768b3967e815af548407f538dfa
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-11 11:49:42 +0200

    game component added.

--- DI_IntroToRxJS_Services/day-two-app/src/app/app.component.ts
+++ DI_IntroToRxJS_Services/day-two-app/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<h1>Hello!  {{title}} </h1>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: ['h1 {color: green}']
 })
 export class AppComponent {
   title = 'day-two-app';
