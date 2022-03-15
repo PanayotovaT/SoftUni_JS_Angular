edbc621f505b58146dacd9c4e9535d1b5540552c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-15 18:39:15 +0200

    Two way binding added.

--- DI_IntroToRxJS_Services/day-two-app/src/app/game/game.component.ts
+++ DI_IntroToRxJS_Services/day-two-app/src/app/game/game.component.ts
@@ -15,13 +15,21 @@ interface Game {
 export class GameComponent {
     shouldShowContent?: boolean;
     
+    searchText: string = 'Game 1';
+
     games: Game[] = [
         { title: 'Minecraft', price:10, img: 'https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png' },
         { title: 'Candy Crush', price: 0, img: 'https://cms.qz.com/wp-content/uploads/2013/09/candy-crush.jpg?quality=75&strip=all&w=450&h=357&crop=1'  },
         { title: 'Counter Strike', price:120, img: 'https://media.moddb.com/images/downloads/1/221/220419/thumb-counter-strike-1.6.jpg'  }
     ]
 
-    public handleExpandContent(): void {
-        this.shouldShowContent = this.shouldShowContent ? false : true;
+    public handleExpandContent(gamesContainer: HTMLElement): void {
+        // this.shouldShowContent = this.shouldShowContent ? false : true;
+        this.shouldShowContent = !this.shouldShowContent;
+        console.log(gamesContainer.children);
+    }
+
+    handleSearchChange(event: Event) {
+        console.log(event);
     }
 }
