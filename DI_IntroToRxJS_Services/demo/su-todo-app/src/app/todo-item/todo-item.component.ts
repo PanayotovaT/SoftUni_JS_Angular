f592786f520dd0d3015614492dce4ac8892963fc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-20 11:33:22 +0200

    Added classe triangle, rectangle, circle

--- DI_IntroToRxJS_Services/demo/su-todo-app/src/app/todo-item/todo-item.component.ts
+++ DI_IntroToRxJS_Services/demo/su-todo-app/src/app/todo-item/todo-item.component.ts
@@ -40,6 +40,44 @@ class Calculator {
   }
 }
 
+interface iShape {
+  calculateArea(): number;
+
+}
+
+class Rectangle implements iShape{
+
+  constructor(public width: number, public height: number) {}
+  
+  calculateArea(): number {
+    return this.width * this.height;
+  }
+}
+
+class Triangle implements iShape {
+  constructor(public base: number, public height: number) {}
+
+  calculateArea(): number {
+      return this.base * this.height / 2;
+  }
+}
+
+class Circle implements iShape {
+
+  constructor(public radius:  number) {}
+
+  calculateArea(): number {
+      return Math.PI * this.radius * this.radius;
+  }
+}
 const calc = new Calculator();
+
 console.log(calc.calculateArea({ base: 6, height: 8 }));
 console.log(calc.calculateArea({ width: 4, height: 8 }));
+const triangle = new Triangle(4, 3);
+const rectangle = new Rectangle(5, 5);
+const circle = new Circle(4);
+
+console.log('Triangle', triangle.calculateArea());
+console.log('Rectangle', rectangle.calculateArea());
+console.log('Circle', circle.calculateArea());
