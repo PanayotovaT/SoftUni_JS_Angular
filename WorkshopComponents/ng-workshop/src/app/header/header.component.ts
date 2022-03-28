14c3ed7bafadbc15b154412213797afb6c357449
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-28 16:21:13 +0300

    login and logut added to the header

--- WorkshopComponents/ng-workshop/src/app/header/header.component.ts
+++ WorkshopComponents/ng-workshop/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLogged(): boolean {
+    return this.userService.isLogged;
   }
+  constructor(public userService: UserService) { }
+
+ loginHandler(): void {
+  this.userService.login()
+ }
+
+ logoutHandler(): void {
+   this.userService.logout()
+}
 
 }
