1a2550cf2d843cbaaadac378f933a6effacebe64
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-06 02:31:49 +0300

    Created a new component.

--- Components/components-app/src/app/app.component.ts
+++ Components/components-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'components-app';
+  welcomeMessage="Hello Alexander!"
 }
