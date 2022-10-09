5db80cac6b2c0b68e82dfd1f051acd9131a9531f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-10 02:06:28 +0300

    Added loadThemes and loadPosts functions

--- WorkshopComponents/components-workshop/src/app/content.service.ts
+++ WorkshopComponents/components-workshop/src/app/content.service.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPost, ITheme } from './shared/interfaces';
 
 @Injectable()
 
 export class ContentService {
 
   constructor(private http: HttpClient) { }
+
   loadThemes() {
-    this.http.get('http://localhost:3000/api/themes')
+    this.http.get<ITheme[]>('http://localhost:3000/api/themes');
   }
+
+  loadPosts() {
+    this.http.get<IPost[]>('http://localhost:3000/api/posts');
+  }
+
 
 }
