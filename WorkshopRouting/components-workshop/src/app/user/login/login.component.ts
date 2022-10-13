8a7d6ab389cccfa29379fc17268f9d6dfd6ea606
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-13 13:59:06 +0300

    Updated the icons in login html.

--- WorkshopRouting/components-workshop/src/app/user/login/login.component.ts
+++ WorkshopRouting/components-workshop/src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +9,8 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export class LoginComponent implements OnInit {
 
   icons = {
-    faEnvelope
+    faEnvelope,
+    faLock
   }
   constructor() { }
 
