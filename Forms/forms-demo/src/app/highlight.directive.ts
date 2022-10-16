2f950bbb936c98b16d8171a76b8c71a237001ce8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-16 13:22:58 +0300

    Added HostBiding.

--- Forms/forms-demo/src/app/highlight.directive.ts
+++ Forms/forms-demo/src/app/highlight.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective{ // implements OnChanges {
 
-  constructor() { }
+  @Input() set isActive(isActive: boolean){
+    if(isActive) {
+      this.color ='red';
+    } else {
+      this.color='black'
+    }
+  }
+
+  @HostBinding('style.color') color ='black';
+
+  //--------------------------------
+  // @Input() isActive!: boolean;
+
+  // constructor(
+  //   private elementRef: ElementRef
+  // ) { }
+
+  // //If we want to have access to the previous value;
+  // // ngOnChanges(simpleChanges: SimpleChanges) : void{
+  // //   if(simpleChanges['isActive'].currentValue) {
+  // //     this.elementRef.nativeElement.style.color = 'red';
+  // //   } else {
+  // //     this.elementRef.nativeElement.style.color = 'black';
+
+  // //   }
+  // // }
+
+  // ngOnChanges() : void{
+  //   if(this.isActive) {
+  //     this.elementRef.nativeElement.style.color = 'red';
+  //   } else {
+  //     this.elementRef.nativeElement.style.color = 'black';
+  //   }
+  // }
 
 }
