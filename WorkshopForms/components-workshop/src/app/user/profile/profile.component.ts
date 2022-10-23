28e057e31bbb884b421d1f7e6f8f8e272ba85079
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-24 01:25:28 +0300

    Added the profile and profile update.

--- WorkshopForms/components-workshop/src/app/user/profile/profile.component.ts
+++ WorkshopForms/components-workshop/src/app/user/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { IUser} from '../../shared/interfaces/user';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,11 +10,22 @@ import { IUser} from '../../shared/interfaces/user';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  user: IUser |null| undefined;
+  inUpdateMode = false;
 
+  get user(){
+    return this.userService.user;
+  }
 
-  constructor(private userService: UserService) {
-    this.user = this.userService.user;
+  constructor(
+    private userService: UserService) { }
+
+   updateProfileForm(form: NgForm): void{
+     if(form.invalid){ return; }
+     this.userService.updateProfile(form.value).subscribe({
+       next: ()=> {
+         this.inUpdateMode = false;
+       },
+       error: (err)=> { console.log(err);}
+     })
    }
-
 }
