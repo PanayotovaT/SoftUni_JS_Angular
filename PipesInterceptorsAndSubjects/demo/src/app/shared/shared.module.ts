b0f798267ab515c9c974d67da837844e70d4b1d1
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-10-24 15:33:52 +0300

    Create get-prop pipe.

--- PipesInterceptorsAndSubjects/demo/src/app/shared/shared.module.ts
+++ PipesInterceptorsAndSubjects/demo/src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoaderComponent } from './loader/loader.component';
+import { GetPropPipe } from './get-prop.pipe';
 
 
 
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    GetPropPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LoaderComponent
+    LoaderComponent,
+    GetPropPipe
+
   ]
 })
 export class SharedModule {
