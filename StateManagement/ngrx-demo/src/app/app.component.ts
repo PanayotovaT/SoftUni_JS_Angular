3bcf10d752366569ba9f3d8623ddc1177f074e08
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-11-03 00:21:46 +0200

    Added a subject to kill subscriptions.

--- StateManagement/ngrx-demo/src/app/app.component.ts
+++ StateManagement/ngrx-demo/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, mapTo, merge, of } from 'rxjs';
+import { map, merge, of, Subject, Subscription, takeUntil } from 'rxjs';
 import { clearAppState, incrementCounter, loadUsers, loadUsersFailure, loadUsersSuccess, setValue } from './+store/actions';
 import { selectGlobalCounter, selectGlobalUsers, selectGlobalValue } from './+store/selector';
 
@@ -10,11 +10,14 @@ import { selectGlobalCounter, selectGlobalUsers, selectGlobalValue } from './+st
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
 
   counter$ = this.store.select(selectGlobalCounter);
   value$ = this.store.select(selectGlobalValue);
   users$ = this.store.select(selectGlobalUsers);
+
+  subscription =  new Subscription();
+  killSubscriptions = new Subject();
 
   isLoadingUsers$ = merge(
     this.actions$.pipe(ofType(loadUsers), map(()=>true)),
@@ -26,7 +29,19 @@ export class AppComponent {
   constructor(
     private store: Store<any>,
     private actions$: Actions
-  ) { }
+  ) {
+    this.subscription.add(
+      this.users$
+      // .pipe(takeUntil(this.killSubscriptions))
+      .subscribe((users)=> console.log(users))
+    );
+    this.subscription.add(
+      this.counter$
+      // .pipe(takeUntil(this.killSubscriptions))
+      .subscribe((counter)=> console.log(counter))
+    );
+
+  }
 
   incrementCounter(): void {
     this.store.dispatch(incrementCounter())
@@ -44,4 +59,11 @@ export class AppComponent {
   resetState(): void {
     this.store.dispatch(clearAppState());
   }
-}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    // this.killSubscriptions.next(this.subscription);
+    // this.killSubscriptions.complete();
+
+  }
+ }
