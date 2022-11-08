82fe80f06fa1ddd1443193686ba25c77fb568218
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-11-09 00:40:57 +0200

    Added craete view.

--- Practice/job-ads/src/app/ads/ads.module.ts
+++ Practice/job-ads/src/app/ads/ads.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdsComponent } from './ads/ads.component';
+import { AdDetailsComponent } from './ad-details/ad-details.component';
+import { CreateAdComponent } from './create-ad/create-ad.component';
 
 
 
 @NgModule({
   declarations: [
-    AdsComponent
+    AdsComponent,
+    AdDetailsComponent,
+    CreateAdComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    AdsComponent,
+    AdDetailsComponent,
   ]
 })
 export class AdsModule { }
--- Practice/job-ads/src/app/app.module.ts
+++ Practice/job-ads/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdsModule } from './ads/ads.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule,
+    UserModule,
+    AdsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
