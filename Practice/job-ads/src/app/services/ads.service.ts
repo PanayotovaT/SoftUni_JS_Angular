acf2f9d9df8af6cbd0330c4ceb4fc4ebfad9b5fd
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-01-16 23:52:31 +0200

    Added loadAllAds method.

--- Practice/job-ads/src/app/services/ads.service.ts
+++ Practice/job-ads/src/app/services/ads.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { iAd } from './interfaces/ad';
+const API_URL = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdsService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  loadAllAds() {
+    return this.http.get <iAd[]>(`${API_URL}/jsonstore/ads`);
+  }
+
 }
