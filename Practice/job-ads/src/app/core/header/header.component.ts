973717862374b5d1550f4c8dffd47f19d7498c3c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-02-21 01:00:40 +0200

    Added logout method to the header comp.

--- Practice/job-ads/src/app/core/header/header.component.ts
+++ Practice/job-ads/src/app/core/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLogged(): boolean {
+    return this.userService.isLogged;
   }
 
+  get email(): string {
+    return this.userService.user?.email || ''
+  }
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  logout(): void {
+    this.userService.logout();
+    this.router.navigate(['/']);
+  }
 }
