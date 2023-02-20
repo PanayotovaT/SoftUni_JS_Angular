f8834e7fc04d5a0e0304f9a987e80b2025a6792d
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-02-21 00:52:33 +0200

    Added the login method and the redirectUrl.

--- Practice/job-ads/src/app/user/login/login.component.ts
+++ Practice/job-ads/src/app/user/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
-  ngOnInit(): void {
+  login(email: string, password: string): void {
+    this.userService.login(email, password);
+    const redirectUrl = this.activatedRoute.snapshot.queryParams['redirectUrl'] || '/';
+    this.router.navigate([redirectUrl]);
+
   }
+
 
 }
