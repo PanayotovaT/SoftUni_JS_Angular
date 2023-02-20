a54b3b1e9d88055ba6bdb39e569c3cfccb63f217
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-02-21 00:44:08 +0200

    Added login and logout to the user service.

--- Practice/job-ads/src/app/services/interfaces/user.ts
+++ Practice/job-ads/src/app/services/interfaces/user.ts
@@ -1,4 +1,4 @@
 export interface IUser {
   'email': string;
-  'skills': string;
+  // 'skills': string;
 }
--- Practice/job-ads/src/app/user/user.service.ts
+++ Practice/job-ads/src/app/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { LocalStorage } from '../core/injection-tokens';
 import { IUser } from '../services/interfaces/user';
 
 @Injectable()
@@ -6,5 +7,31 @@ export class UserService {
 
   user: IUser | undefined;
 
-  constructor() { }
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(@Inject(LocalStorage) private localStorage: Window['localStorage']) {
+    try {
+      const localStorageUser = this.localStorage.getItem('<USER>') || 'ERROR';
+      this.user = JSON.parse(localStorageUser);
+
+    } catch (err) {
+      this.user = undefined;
+    }
+
+  }
+
+  login(email: string, password: string): void {
+    this.user = {
+      email
+    };
+
+    this.localStorage.setItem('<USER>', JSON.stringify(this.user));
+  }
+
+  logout(): void {
+    this.user = undefined;
+    this.localStorage.removeItem('<USER>');
+ }
 }
