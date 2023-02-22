aba3972ab21318d531cc95db05b632e8ac7a94cf
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-02-23 00:01:37 +0200

    Updated the visibility of the header tabs.

--- Practice/job-ads/src/app/core/core.module.ts
+++ Practice/job-ads/src/app/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { LocalStorage } from './injection-tokens';
 
 
 
@@ -17,6 +18,40 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule,
     SharedModule
   ],
+  providers: [{
+    provide: LocalStorage,
+    useFactory: (platformId: object) => {
+      if (isPlatformBrowser(platformId)) {
+        return window.localStorage;
+      }
+
+      if (isPlatformServer(platformId)) {
+        return class implements Storage {
+          length = 0;
+          private data: Record<string, string> = {};
+
+          clear(): void {
+            this.data = {}
+          }
+          getItem(key: string): string | null {
+            return this.data[key];
+          }
+          key(index: number): string | null {
+            throw new Error('Method not implemented.');
+          }
+          removeItem(key: string): void {
+            const { [key]: removedItem, ...others } = this.data;
+            this.data = others;
+          }
+          setItem(key: string, value: string): void {
+            this.data[key] = value
+          }
+        }
+      }
+      throw Error('Not Found');
+    },
+    deps: [PLATFORM_ID]
+  }],
   exports: [
     HeaderComponent,
     FooterComponent,
