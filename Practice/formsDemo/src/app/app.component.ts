de740f4a90335517bbb5ea3a4d3d267b2d051eb5
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-04-25 12:15:12 +0300

    Added a validation for the selected input.

--- Practice/formsDemo/src/app/app.component.ts
+++ Practice/formsDemo/src/app/app.component.ts
@@ -7,7 +7,18 @@ import { User } from './user';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  languages: string[] = ['JavaScript', 'Angular', 'React'];
+  languages: string[] = ['Select a Language','JavaScript', 'Angular', 'React'];
+  languageHasError: boolean = true;
 
   userModel = new User('', 'Bg', 'Sofia', '1000', 'JavaScript', 'evening', 'alex@gmail.com', true);
+
+  validateLanguage(value: string): void {
+    if (value === 'Select a Language'){
+      this.languageHasError = true;
+    } else {
+      this.languageHasError = false;
+
+    }
+  }
+
 }
