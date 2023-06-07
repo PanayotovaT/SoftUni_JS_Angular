0f07f9358d9faef24a36590862d662c559aa77b5
fortalesi <75318821+fortalesi@users.noreply.github.com>
2023-06-07 16:06:26 +0300

    added formBuilder service, refactured the code.

--- Practice/reactive-form/src/app/app.component.ts
+++ Practice/reactive-form/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +7,88 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-registrationForm =  new FormGroup({
-  username: new FormControl('Alexander'),
-  password: new FormControl(''),
-  confirmPassword: new FormControl(''),
-  address: new FormGroup({
-    city: new FormControl(''),
-    state: new FormControl(''),
-    postalCode: new FormControl('')
+
+constructor(private fb: FormBuilder){}
+  registrationForm = this.fb.group({
+    username: ['Alexander'],
+    password: [''],
+    confirmPassword: [''],
+    address:  this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
   })
 
-});
 
-loadData(){
-this.registrationForm.setValue({
-  username: 'Sofia',
-  password: '123',
-  confirmPassword: '123',
-  address: {
-    city: 'Milan',
-    state: 'Italy',
-    postalCode: '123456'
+
+//SETVALUE -need to provide all the values
+  // loadData() {
+  //   this.registrationForm.setValue({
+  //     username: 'Sofia',
+  //     password: '123',
+  //     confirmPassword: '123',
+  //     address: {
+  //       city: 'Milan',
+  //       state: 'Italy',
+  //       postalCode: '123456'
+  //     }
+  //   })
+  // }
+
+  //PATCHVALUE -- update just some fields
+  loadData() {
+    this.registrationForm.patchValue({
+      username: 'Sofia',
+      password: '123',
+      confirmPassword: '123',
+    })
   }
-})
 }
 
-}
+
+//MANUALY creating form controls
+
+// import { Component } from '@angular/core';
+// import { FormControl, FormGroup } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   registrationForm = new FormGroup({
+//     username: new FormControl('Alexander'),
+//     password: new FormControl(''),
+//     confirmPassword: new FormControl(''),
+//     address: new FormGroup({
+//       city: new FormControl(''),
+//       state: new FormControl(''),
+//       postalCode: new FormControl('')
+//     })
+
+//   });
+// //SETVALUE -need to provide all the values
+//   // loadData() {
+//   //   this.registrationForm.setValue({
+//   //     username: 'Sofia',
+//   //     password: '123',
+//   //     confirmPassword: '123',
+//   //     address: {
+//   //       city: 'Milan',
+//   //       state: 'Italy',
+//   //       postalCode: '123456'
+//   //     }
+//   //   })
+//   // }
+
+//   //PATCHVALUE -- update just some fields
+//   loadData() {
+//     this.registrationForm.patchValue({
+//       username: 'Sofia',
+//       password: '123',
+//       confirmPassword: '123',
+//     })
+//   }
+// }
